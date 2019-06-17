ea57524dbe6ae0af23417c42f36dd72f474c3546
brunosbr <51544740+brunosbr@users.noreply.github.com>
2019-06-16 23:41:38 -0300

    Update config.py
    json de congiguracion

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import PySimpleGUI as sg
 import random
 import string
+import json
 from pattern.web import Wiktionary
 from pattern.es import verbs, conjugate, INFINITIVE, parse, parsetree, tokenize,tag
 from pattern.search import search
@@ -40,19 +41,20 @@ def analizarpalabra(palabra,cat):
 layout = [
 			[sg.Text('Instrucciones de configuracion')],
 			[sg.Text('Palabra:')],
-			[sg.Radio('Adjetivo', "RADIOp",key='esAdj'),
-			 sg.Radio('Verbo', "RADIOp",key='esVer'),
-			 sg.Radio('Sustantivo', "RADIOp",key='esSus')],
+			[sg.Radio('Sustantivo', "RADIOp",default = True,key='esSus'),
+			 sg.Radio('Adjetivo', "RADIOp",key='esAdj'),
+			 sg.Radio('Verbo', "RADIOp",key='esVer')],
+			 
 			[sg.Input(key='_IN_')],
 			[sg.Button('Agregar',key='_ADD_')],
 			[sg.Text('Ayudas:')],
-			[sg.Radio('Sin ayuda', "RADIOA", size=(10,1)),
-			 sg.Radio('Definiciones', "RADIOA"),
-			 sg.Radio('Mostrar palabras', "RADIOA")],
+			[sg.Radio('Sin ayuda', "RADIOA",default = True, key= 'sin', size=(10,1)),
+			 sg.Radio('Definiciones', "RADIOA", key='defin'),
+			 sg.Radio('Mostrar palabras', "RADIOA", key='pal')],
 			 
-			[sg.Text('Orientacion')],
-			[sg.Radio('Horizontal', "RADIOH", size=(10,1)),
-			 sg.Radio('Vertical', "RADIOH")],
+			[sg.Text('Orientacion:')],
+			[sg.Radio('Horizontal', "RADIOH",default = True, key='hor', size=(10,1)),
+			 sg.Radio('Vertical', "RADIOH", key='ver'), sg.Radio('Mixto', "RADIOH", key='mix')],
 			
 			[sg.Text('Cantidad de Palabras')],
 			[sg.Text('Adjetivo'),sg.Input(do_not_clear=True, key='_CANT_ADJ_')],
@@ -60,17 +62,19 @@ layout = [
 			[sg.Text('Sustantivo'),sg.Input(do_not_clear=True, key='_CANT_SUS_')],
 			
 			[sg.Text('Mayus')],
-			[sg.Radio('Mayúscula', "RADIOn", size=(10,1)),
-			 sg.Radio('Minúscula', "RADIOn")],
+			[sg.Radio('Mayúscula', "RADIOn",default = True, key='mayus', size=(10,1)),
+			 sg.Radio('Minúscula', "RADIOn", key='minus')],
 			 
    			[sg.Text('Fuente')],
    			[sg.InputCombo(('Arial','Courier','Comic','Fixedsys','Times','Verdana','Helvetica'), key='_FONT_')],
 			[sg.Text('Oficina')],
-			[sg.Button('Cerrar')]
+			[sg.Button('Aceptar', key='_ACEPTAR_'),sg.Button('Cerrar')]
 			]
 window = sg.Window('CONFIG GUI').Layout(layout)
 window.Finalize()
 
+config={}
+palabras={}
 while True:                 # Event Loop  
 	event, values = window.Read()  
 	#print(event, val)
@@ -80,5 +84,19 @@ while True:                 # Event Loop
 		print('values',values)
 		cat={'esAdj':values['esAdj'],'esVer':values['esVer'],'esSus':values['esSus']}
 		analizarpalabra(values['_IN_'],cat)
-
+		palabras[values['_IN_']]={'tipo': 'adj' if values['esAdj']==True else 'verb' if values['esVer']==True else 'sust', 'def':''}
+		#print(palabras)
+	if event == '_ACEPTAR_':
+		config['palabras']=palabras	
+		config['ayuda']= "sin ayuda" if values['sin']==True else "definiciones" if values['defin']==True else "palabras" 
+		config['orientacion'] = "horizontal" if values['hor']==True else "vertical" if values['ver']==True else "mixto"
+		config['mayuscula'] = True if values['mayus']==True else False
+		config['fuente'] = values['_FONT_']
+		for key in config:
+			print(key, '=',config[key])
+		break	
 window.Close()
+with open('configuracion.json', 'w') as f:
+	json.dump(config, f)
+
+
